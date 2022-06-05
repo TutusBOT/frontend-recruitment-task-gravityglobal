df597fe645537ec007b80266f28ab51b559514c6
TutusBOT <barte77pl@gmail.com>
2022-06-05 02:01:18 +0200

    feat: click counter is now saved in localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,6 @@ const button = document.getElementById("button");
 const popup = document.getElementById("popup");
 const counter = document.getElementById("module-counter");
 const closeButton = document.querySelector(".module__close-button");
+const resetButton = document.getElementById("module-reset-button");
 
-createPopup(button, popup, counter, closeButton);
+createPopup(button, popup, counter, closeButton, resetButton);
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,17 +1,24 @@
-function createPopup(button, popup, counter, closeButton) {
+function createPopup(button, popup, counter, closeButton, resetButton) {
 	let count = 0;
+	if (localStorage.getItem("popupCounter")) {
+		count = parseInt(localStorage.getItem("popupCounter"));
+	}
 	button.addEventListener("click", () => {
 		count++;
+		localStorage.setItem("popupCounter", count);
 		counter.innerText = `${count} times`;
 		if (count > 5) {
-			const popupResetButton = document.createElement("button");
+			resetButton.style.display = "block";
 		}
 		popup.style.display = "grid";
 	});
-	popup.addEventListener("click", (e) => {
-		// popup.style.display = "none";
-	});
 	closeButton.addEventListener("click", (e) => {
 		popup.style.display = "none";
+	});
+	resetButton.addEventListener("click", () => {
+		count = 0;
+		localStorage.setItem("popupCounter", count);
+		counter.innerText = `${count} times`;
+		resetButton.style.display = "none";
 	});
 }
