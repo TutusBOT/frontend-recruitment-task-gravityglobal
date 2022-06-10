f5434d01ba4f01317b534945c5755c3b19acdd19
TutusBOT <barte77pl@gmail.com>
2022-06-10 19:57:16 +0200

    feat: added babel and webpack

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
-const button = document.getElementById("button");
+import create from "./components/popup.js";
+
+const openButton = document.getElementById("button");
 const popup = document.getElementById("popup");
 const counter = document.getElementById("module-counter");
 const closeButton = document.querySelector(".module__close-button");
 const resetButton = document.getElementById("module-reset-button");
 
-createPopup(button, popup, counter, closeButton, resetButton);
+create(popup, counter, openButton, closeButton, resetButton);
