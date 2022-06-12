88142f1c7e093903a403e9fd66e4b70695d9137a
TutusBOT <barte77pl@gmail.com>
2022-06-12 22:18:22 +0200

    chore: removed unnecessary code

--- src/js/components/button.js
+++
@@ -1,7 +0,0 @@
-export default function button(text, className, callback) {
-	const button = document.createElement("button");
-	button.innerText = text;
-	button.classList.add(className);
-	button.addEventListener("click", callback);
-	return button;
-}
