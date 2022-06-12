bcec41d081fea3d954eeaf092d57d09173fb6888
TutusBOT <barte77pl@gmail.com>
2022-06-12 22:34:16 +0200

    fix: improved accessibility

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,15 +1,21 @@
 import createElement from "../helpers/createElement";
 
-export default function Modal(attr = {}, child = []) {
-	const modal = createElement("div", { class: "modal", ...attr }, [child]);
+export default function Modal(prevFocusedElement, attr = {}, child = []) {
+	const modal = createElement(
+		"div",
+		{ class: "modal", role: "dialog", ...attr },
+		[child]
+	);
 	document.addEventListener("click", (e) => {
 		if (e.target === modal) {
 			modal.style.display = "none";
+			prevFocusedElement.focus();
 		}
 	});
 	document.addEventListener("keydown", (e) => {
 		if (e.key === "Escape") {
 			modal.style.display = "none";
+			prevFocusedElement.focus();
 		}
 	});
 	return modal;
