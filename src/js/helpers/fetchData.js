d3e7ada43a605935416d96b97676f6c440de7d36
TutusBOT <barte77pl@gmail.com>
2022-06-12 16:18:50 +0200

    fix: remove unnecessary code

--- src/js/helpers/fetchData.js
+++ src/js/helpers/fetchData.js
@@ -1,9 +1,5 @@
 export default async function fetchData(endpoint) {
-	try {
-		const response = await fetch(endpoint);
-		if (!response.ok) throw new Error(response.statusText);
-		return await response.json();
-	} catch (err) {
-		throw new Error(err);
-	}
+	const response = await fetch(endpoint);
+	if (!response.ok) throw new Error(response.statusText);
+	return await response.json();
 }
