317f1c6adf35181ce0721d1248dacd87622785cf
TutusBOT <barte77pl@gmail.com>
2022-06-12 22:05:38 +0200

    feat: added accessibilty improvement, use helper to generate modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,8 +1,14 @@
-export default function modal() {
-	const modal = document.createElement("div");
-	modal.classList.add("modal");
+import createElement from "../helpers/createElement";
+
+export default function Modal(attr = {}, child = []) {
+	const modal = createElement("div", { class: "modal", ...attr }, [child]);
 	document.addEventListener("click", (e) => {
 		if (e.target === modal) {
+			modal.style.display = "none";
+		}
+	});
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Escape") {
 			modal.style.display = "none";
 		}
 	});
